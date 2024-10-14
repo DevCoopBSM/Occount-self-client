a9fcfe0be9cb4eeb1670424591f20dca28d1f6a2
hyeonjoonpark <pjjoon1379@gmail.com>
2024-10-14 12:22:10 +0900

    FIX: 안정화타이머 대신 연속값으로 판정

--- lib/services/person_counter_service.dart
+++ lib/services/person_counter_service.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'dart:convert';
 import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:audioplayers/audioplayers.dart';
@@ -6,10 +5,9 @@ import 'package:audioplayers/audioplayers.dart';
 class PersonCounterService {
   late WebSocketChannel _channel;
   final AudioPlayer _audioPlayer = AudioPlayer();
-  double _lastStableAvgCount = 0;
   double _currentAvgCount = 0;
-  Timer? _stabilityTimer;
   bool _isPlaying = false;
+  bool hasEntered = false;  // 사람이 들어왔는지 여부
 
   PersonCounterService() {
     _initWebSocket();
@@ -20,7 +18,6 @@ class PersonCounterService {
       _channel = WebSocketChannel.connect(
         Uri.parse('wss://occount.bsm-aripay.kr/ws/person_count'),
       );
-      print('WebSocket connected successfully');
       _channel.stream.listen(
         _handleMessage,
         onError: (error) {
@@ -36,30 +33,37 @@ class PersonCounterService {
   }
 
   void _handleMessage(dynamic message) {
-    print('Received message: $message'); // 디버깅을 위한 출력
     try {
       final Map<String, dynamic> data = jsonDecode(message);
       _currentAvgCount = (data['avg_count'] as num).toDouble();
       print('Current average count: $_currentAvgCount');
 
-      // 안정화 타이머 재설정
-      _stabilityTimer?.cancel();
-      _stabilityTimer = Timer(Duration(seconds: 2), _checkStableCount);
+      // 안정화 타이머 비활성화
+      //_stabilityTimer?.cancel();
+      //_stabilityTimer = Timer(Duration(seconds: 2), _checkStableCount);
+
+      _checkStableCount();  // 바로 실행해보기
     } catch (e) {
       print('Error handling message: $e');
     }
   }
 
   void _checkStableCount() {
-    print('Checking stable count: $_currentAvgCount vs $_lastStableAvgCount');
-    if (_currentAvgCount > _lastStableAvgCount && _currentAvgCount >= 1) {
+    print('Checking stable count: $_currentAvgCount');
+
+    // 0이 아닌 값이 들어왔을 때 "어서오세요"를 한 번만 재생
+    if (_currentAvgCount != 0 && !hasEntered) {
       print('Playing welcome message');
       _playWelcomeMessage();
-    } else if (_currentAvgCount < _lastStableAvgCount && _currentAvgCount == 0) {
+      hasEntered = true;  // 사람이 들어왔으므로 "어서오세요"는 더 이상 재생하지 않음
+    }
+
+    // 0이 되었을 때 "안녕히 가세요" 메시지 재생하고 다시 "어서오세요" 가능 상태로 초기화
+    else if (_currentAvgCount == 0 && hasEntered) {
       print('Playing goodbye message');
       _playGoodbyeMessage();
+      hasEntered = false;  // 사람이 나갔으므로 "어서오세요"를 다시 재생할 수 있게 함
     }
-    _lastStableAvgCount = _currentAvgCount;
   }
 
   Future<void> _playWelcomeMessage() async {
@@ -98,6 +102,6 @@ class PersonCounterService {
     print('Disposing PersonCounterService');
     _channel.sink.close();
     _audioPlayer.dispose();
-    _stabilityTimer?.cancel();
+    //_stabilityTimer?.cancel();
   }
 }
