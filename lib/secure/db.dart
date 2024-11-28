16738ccfef8c1b53a67cd5898098ed9a6475c2b5
Hwansi <hwansi@bssm.hs.kr>
2024-11-28 09:43:11 +0900

    UPDATE: 시연용 공개주소

--- lib/secure/db.dart
+++ lib/secure/db.dart
@@ -1,9 +1,7 @@
 class DbSecure {
   final String dbHost;
 
-  DbSecure()
-      : dbHost = const String.fromEnvironment('DB_HOST',
-            defaultValue: 'http://localhost:8080');
+  DbSecure() : dbHost = 'https://kiosk.bsm-aripay.kr';
 
   // DB_HOST getter 추가
   // ignore: non_constant_identifier_names
